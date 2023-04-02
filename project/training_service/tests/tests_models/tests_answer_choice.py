dc60a951ab5e3fd82d121df3b0638997f3c13e83
Sergey Alhimenko <sergeyalhimenko@Sergeys-Air.lan>
2023-04-03 00:54:09 +0700

    rename classes

--- project/training_service/tests/tests_models/tests_answer_choice.py
+++ project/training_service/tests/tests_models/tests_answer_choice.py
@@ -1,7 +1,7 @@
 from training_service.tests.base import Base
 
 
-class CommentTopic(Base):
+class TestAnswerChoice(Base):
     def test__str__(self):
         answer = self.answer_choices[self.question][True]
         self.assertEqual(str(answer), answer.text)
--- project/training_service/tests/tests_models/tests_comment.py
+++ project/training_service/tests/tests_models/tests_comment.py
@@ -2,7 +2,7 @@ from training_service.models import Comment
 from training_service.tests.base import PreBase
 
 
-class CommentTopic(PreBase):
+class TestComment(PreBase):
     def test__str__(self):
         comment = Comment.objects.create(question=self.question, text="Comment text")
         self.assertEqual(str(comment), comment.text)
